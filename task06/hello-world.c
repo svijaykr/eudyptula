eaeddf8be783df1d085293f750c5061540330107
Koray Gulcu <kabexxnuf@gmail.com>
2014-05-18 20:51:50 +0100

    Implement read and write routines

--- task06/hello-world.c
+++ task06/hello-world.c
@@ -2,19 +2,24 @@
 #include <linux/init.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
-
+#include <linux/kernel.h>
+#include <asm/uaccess.h>
+#include <linux/errno.h>
 
 static int hello_open(struct inode *inp, struct file *fp);
 static int hello_release(struct inode *inp, struct file *fp);
 static ssize_t hello_read(struct file *fp, char __user *user, size_t size, 
 			loff_t *offs);
 
+static ssize_t hello_write(struct file *fp, const char __user *user, size_t size,
+			loff_t *offs);
 
 static const struct file_operations hello_fops = {
         .owner = THIS_MODULE,
         .read = hello_read,
 	.open = hello_open,
 	.release = hello_release,
+	.write = hello_write
 	};
 
 
@@ -49,23 +54,52 @@ static void __exit hello_exit(void)
 module_exit(hello_exit);
 
 
-static int hello_open(struct inode *inp, struct file *fp)
+int hello_open(struct inode *inp, struct file *fp)
 {
-	pr_info(">> hello_open()");
+	pr_debug(">> hello_open()");
 	return 0;
 }
 
-static int hello_release(struct inode *inp, struct file *fp)
+int hello_release(struct inode *inp, struct file *fp)
 {
-	pr_info(">> hello_release()");
+	pr_debug(">> hello_release()");
 	return 0;
 }
 
-static ssize_t hello_read(struct file *fp, char __user *user, size_t size, 
+ssize_t hello_read(struct file *fp, char __user *user, size_t size, 
 			loff_t *offs)
 {
-	pr_info(">> hello_read()");
-	return 0;
+	char buf[4] = "";
+	pr_debug(">> hello_read()");
+	sprintf(buf,"%d\n", hello_misc.minor);
+	pr_debug("Sending %s", buf);
+
+	if((*offs) == (strlen(buf) + 1))
+		return 0;
+	else if(!copy_to_user(user, buf, (strlen(buf) + 1)))
+		return ((*offs) += (strlen(buf) + 1));
+	else
+		return EAGAIN;
+}
+
+ssize_t hello_write(struct file *fp, const char __user *user, size_t size,
+			loff_t *offs)
+{
+	char buf_mine[4] = "";
+	char buf_arg[4] = "";
+	pr_debug(">> hello_write()");
+	sprintf(buf_mine, "%d", hello_misc.minor);
+	pr_debug("size:%lu ", size);
+	pr_debug("offset:%lld ", (*offs));
+
+	if ((strlen(buf_mine) == (size - 1)) && 
+		!copy_from_user(buf_arg, user, strlen(buf_mine) + 1)){
+		pr_debug("received: %s", buf_arg);
+		pr_debug("mine: %s", buf_mine);
+		return strncmp(buf_mine, buf_arg, strlen(buf_mine)) ? -EINVAL :
+			size;
+	} else
+		return -EINVAL;
 }
 
 MODULE_LICENSE("GPL");
